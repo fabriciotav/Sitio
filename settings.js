9f6470ba8d815c891f2d0a68f851820f2f1b44ba
Fabricio Tavares de Oliveira <fabriciotavareso@gmail.com>
2013-02-18 13:40:04 -0300

    Update to use vhost

---
+++ settings.js
@@ -0,0 +1,33 @@
+exports.routes = function(app) {
+
+    app.use(express.static('assets'));
+
+    app.engine('.html', require('ejs').__express);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
+
+
+    app.get('/', function( req, res) {
+        res.render('index');
+    });
+    
+    app.get('/acomodacoes', function( req, res) {
+        res.render('acomodacoes');
+    });
+    
+    app.get('/como-chegar', function( req, res) {
+        res.render('como-chegar');
+    });
+    
+    app.get('/contato', function( req, res) {
+        res.render('contato');
+    });
+    
+    app.get('/lazer', function( req, res) {
+        res.render('lazer');
+    });
+    
+    app.get('/reservas', function( req, res) {
+        res.render('reservas');
+    });
+};
